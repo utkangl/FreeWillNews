60d9bdf54d112682ee88a4139e7e78c53ec43b17
utkan <utkngl@gmail.com>
2023-05-01 19:54:46 +0300

    some of the pages created as proper html files

--- FreeWillNews/FreeWillNews/settings.py
+++ FreeWillNews/FreeWillNews/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -110,3 +111,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
